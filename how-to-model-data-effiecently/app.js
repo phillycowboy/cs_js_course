e4af9fe9951f9b2b0331e289edab28ef7e9d3b88
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-03-05 11:26:07 -0500

    playing around

---
+++ how-to-model-data-effiecently/app.js
@@ -0,0 +1,17 @@
+// let string = "Hello there"
+// let scream = document.getElementsByTagName('h1')
+
+//  document.body.innerText = string
+
+//  function handleClickEvent() {
+//      console.log('stop clicking!');
+//      alert("ITS WORKING!")
+ 
+//  }
+
+//  function handleScream() {
+//      this.style.color = 'red';
+//  }
+
+//  document.addEventListener('click', handleClickEvent)
+//  document.addEventListener('click', handleScream)
