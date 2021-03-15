f38a2045b100448ec4a446a10ebc10f5d663deae
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-03-15 14:30:10 -0400

    code challenge with functions

--- functions/app5.js
+++ functions/app5.js
@@ -66,19 +66,159 @@ function avg(firstNum, secondNum) {
 function square(num) {
     console.log(num * num);
 }
-square(6);
+// square(6);
 
 function sum(x,y) {
     console.log(x + y);
 }
-sum(5,4);
+// sum(5,4);
 
 function divide(a,b) {
     console.log(a/b);
 }
-divide(5,10);
+// divide(5,10);
 
 
 // THE RETURN STATEMENT 
 
 
+const scream = 'hello'.toUpperCase();
+
+
+function add(x, y) {
+    return x + y;
+}
+
+// you can only return one thing from a function 
+// what we can do with the return value in add is store it to a variable so: 
+// const total = add(3,4)
+// then we get 7 when we call total. 
+
+// function isPurple(color) {
+//     if(color.toLowerCase() === 'purple'){
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
+
+// function isPurple(color) {
+//     if (color.toLowerCase() === 'purple') {
+//         return true;
+//     }
+//     return false;
+    
+// }
+
+// function isPurple(color) {
+//     return color.toLowerCase() === 'purple'; 
+
+// }
+
+function containsPurple(arr){
+    for(let color of arr){
+        if(color.toLowerCase() === 'purple' || color.toLowerCase() === 'magenta'){
+            return true;
+        }
+    }
+    return false; 
+}
+
+// PRACTICE 
+// isValidPassword function 
+
+// function isValidPassword(password, username) {
+//     if(password.length < 8 ){
+//         return false;
+//     }
+//     if(password.indexOf(' ') !== -1){
+//         return false; 
+//     }
+//     if(password.indexOf(username) !== -1){
+//         return false;
+//     }
+
+//     return true; 
+// }
+
+// refactor 
+
+// function isValidPassword(password, username) {
+//     if (
+//         password.length < 8 ||
+//         password.indexOf(' ') !== -1 || 
+//         password.indexOf(username) !== -1
+//         ){
+//         return false;
+//     }
+//     return true; 
+// }
+
+// function isValidPassword(password, username) {
+//     const tooShort = password.length < 8;
+//     const noSpace = password.indexOf(' ') !== -1;  
+//     const notUsername = password.indexOf(username) !== -1;
+//     if(tooShort || noSpace || notUsername){
+//         return false;
+//     }
+    
+//     return true; 
+// }
+
+// function isValidPassword(password, username) {
+//     const tooShort = password.length < 8;
+//     const noSpace = password.indexOf(' ') !== -1;  
+//     const notUsername = password.indexOf(username) !== -1;
+//     if(!tooShort && !noSpace && !notUsername){
+//         return true;
+//     }
+//     return false; 
+// }
+
+function isValidPassword(password, username) {
+    const tooShort = password.length < 8;
+    const noSpace = password.indexOf(' ') !== -1;  
+    const notUsername = password.indexOf(username) !== -1;
+    return !tooShort && !noSpace && !notUsername;
+     
+}
+
+// CODE CHALLENGE AVERAGE NUMBER IN AN ARRAY 
+
+function averageNumber(array) {
+    let total = 0; 
+    // loop over each number 
+    for(let num of array){
+        // add them together 
+        total += num; 
+    }
+    // divide by number of nums 
+    return total/array.length;
+}
+
+
+// CODE CHALLENGE MATCHING A SENTENCE TO SEE IF ITS A PANGRAM 
+
+// function isPangram(string) {
+//     let lowerCased = string.toLowerCase();
+//     for(let char of 'abcdefghijklmnopqrstuvwxyz')
+//     if (lowerCased.indexOf(char) === -1) {
+//         return false;
+//     }
+//     return true;
+// }
+
+function isPangram(string) {
+    let lowerCased = string.toLowerCase();
+    for(let char of 'abcdefghijklmnopqrstuvwxyz')
+    if (!lowerCased.includes(char)) {
+        return false;
+    }
+    return true;
+}
+
+// function isPangram(string) {
+//     const regex = /([a-z])(?!>*\1)/g;
+//     return (string.match(regex) || []).length === 26;
+// }
