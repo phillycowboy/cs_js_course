2b1574c9080e20d607dbbc57f30506a2a328ef39
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-03-16 17:16:04 -0400

    started forEach

--- functions-to-collections-of-data/app7.js
+++ functions-to-collections-of-data/app7.js
@@ -2,3 +2,11 @@ const btn = document.querySelector('button');
 btn.addEventListener('click', function (){
     alert('YEAH THE PAGES ARE LINKED!');
 })
+
+// FOREACH -- 
+
+const nums = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+
+nums.forEach(function (n) {
+    console.log(n * n);
+});
