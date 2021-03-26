ff57b47b9606be45e520ab961f991fea1cfb2837
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-03-26 12:56:35 -0400

    made table for NBA scores

--- twisting-the-dom-to-our-will/nba-score-chart-dom-project/app.js
+++ twisting-the-dom-to-our-will/nba-score-chart-dom-project/app.js
@@ -1,3 +1,101 @@
-const h1 = document.createElement('h1');
-h1.innerText = "NBAJAM";
-document.body.append(h1);
+// const h1 = document.createElement('h1');
+// h1.innerText = "NBAJAM";
+// document.body.append(h1);
+
+const warriorGames = [{
+    awayTeam:{
+        team: 'Golden State',
+        points: 119,
+        isWinner: true
+    },
+    homeTeam: {
+        team: 'Houston',
+        points: 106,
+        isWinner: false 
+    }
+
+},
+{
+    awayTeam:{
+        team: 'Houston',
+        points: 105,
+        isWinner: false
+    },
+    homeTeam: {
+        team: 'Golden State',
+        points: 127,
+        isWinner: true 
+    }
+},
+{
+    awayTeam:{
+        team: 'Golden State',
+        points: 133,
+        isWinner: true
+    },
+    homeTeam: {
+        team: 'Houston',
+        points: 127,
+        isWinner: false 
+    }
+},
+{
+    awayTeam:{
+        team: 'Houston',
+        points: 100,
+        isWinner: true
+    },
+    homeTeam: {
+        team: 'Golden State',
+        points: 99,
+        isWinner: false 
+    }
+},
+{
+    awayTeam:{
+        team: 'Golden State',
+        points: 99,
+        isWinner: false
+    },
+    homeTeam: {
+        team: 'Houston',
+        points: 127,
+        isWinner: true  
+    }
+},
+{
+    awayTeam:{
+        team: 'Houston',
+        points: 150,
+        isWinner: true
+    },
+    homeTeam: {
+        team: 'Golden State',
+        points: 127,
+        isWinner: false 
+    }
+}
+]
+
+const ulParent = document.createElement('ul');
+for(let game of warriorGames){
+    const {homeTeam, awayTeam} = game;
+    const gameLi = document.createElement('li');
+    const {team: hTeam, points: hPoints} = homeTeam;
+    const {team: aTeam, points: aPoints} = awayTeam;
+    const teamNames  = `${aTeam} @ ${hTeam}`;
+    let scoreLine
+    if (aPoints > hPoints){
+         scoreLine = `<b>${aPoints}</b> - ${hPoints}`;
+    }else {
+         scoreLine = `${aPoints} - <b>${hPoints}</b>`;
+    }
+
+    const warriors = hTeam === 'Golden State' ? homeTeam : awayTeam;
+    gameLi.classList.add(warriors.isWinner ? 'win' : 'loss');
+    
+    
+    gameLi.innerHTML = `${teamNames} ${scoreLine}`;
+    ulParent.append(gameLi);
+}
+document.body.prepend(ulParent);
