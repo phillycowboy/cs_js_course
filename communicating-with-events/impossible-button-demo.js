b0ce0e7db55b326c95216af952d06e019d19b57e
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-03-26 16:31:42 -0400

    fin impossible button demo

--- communicating-with-events/impossible-button-demo.js
+++ communicating-with-events/impossible-button-demo.js
@@ -8,3 +8,21 @@ h1.addEventListener('mouseout', function(){
     this.style.fontSize = '60px';
     this.style.color = 'slategrey'
 });
+
+
+const btn = document.querySelector('button');
+// window.screen in console will give you dimesions 
+// window.innerHeight
+// window.innerWidth
+btn.addEventListener('mouseover', function(){
+    console.log("MOUSED OVER ME");
+    const height = Math.floor(Math.random() * window.innerHeight)
+    const width = Math.floor(Math.random() * window.innerWidth)
+    btn.style.left = `${height}px`;
+    btn.style.top = `${width}px`;
+})
+
+btn.addEventListener('click', function(){
+    btn.innerText = 'YOU GOT ME!';
+    document.body.style.backgroundColor = 'green';
+})
