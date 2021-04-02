21f960e6c5f199c766fc4c3fb473b9b4999cf51a
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-04-02 11:15:21 -0400

    notes

--- communicating-with-events/form-events-and-prevent-default/app.js
+++ communicating-with-events/form-events-and-prevent-default/app.js
@@ -1,7 +1,15 @@
-const h1 = document.querySelector('h1');
-h1.addEventListener('mouseover', () => {
-    document.body.style.backgroundColor = 'pink';
-})
-h1.addEventListener('mouseout', () => {
-    document.body.style.backgroundColor = 'white';
-})
+// window.addEventListener('DOMContentLoaded', function(){
+//     const h1 = document.querySelector('h1');
+//     h1.addEventListener('mouseover', () => {
+//         document.body.style.backgroundColor = 'pink';
+//         h1.style.fontSize = '600px';
+//     })
+//     h1.addEventListener('mouseout', () => {
+//         document.body.style.backgroundColor = 'white';
+//         h1.style.fontSize = '2em';
+//     })
+
+// })
+
+
+
