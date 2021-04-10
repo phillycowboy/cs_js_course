fab873f637d46fec115366387ecd7c24d774147b
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-04-10 15:22:00 -0400

    ?

--- async-code-callbacks-promises/welcome-to-callback-hell/app.js
+++ async-code-callbacks-promises/welcome-to-callback-hell/app.js
@@ -1,1 +1,108 @@
 // alert('the pages are connected!');
+
+// callbacks are not perfect they can be messy, this will be a callback example, and then we'll refactor it using promises. 
+
+const btn = document.querySelector('button');
+
+// setTimeout(() => {
+//     btn.style.transform = `translateX(100px)`; 
+//     setTimeout(() => {
+//         btn.style.transform = `translateX(200px)`;
+//         setTimeout(() => {
+//             btn.style.transform = `translateX(300px)`;
+//             setTimeout(() => {
+//                 btn.style.transform = `translateX(400px)`;
+//                 setTimeout(() => {
+//                     btn.style.transform = `translateX(500px)`;
+//                 }, 1000);
+//             }, 1000);
+//         }, 1000);
+//     }, 1000);
+// }, 1000);
+
+// const moveX = (element, amount, delay, callback) => {
+//     const bodyBoundary = document.body.clientWidth;
+//     const elRight =element.getBoundingClientRect().right;
+//     const currentLeft = element.getBoundingClientRect().left;
+//     if(elRight + amount > bodyBoundary){
+//         console.log('DONE! - CANNOT GO THAT FAR!');
+//     }else {
+//         setTimeout(() => {
+//             element.style.transform = `translateX(${currentLeft + amount}px)`;
+//             if (callback) callback();
+//         }, delay);
+//     };
+// };
+
+// btn.getBoundingClientRect();
+// DOMRect { x: 508, y: 149.375, width: 74.53125, height: 21, top: 149.375, … }
+// bottom: 170.375
+// height: 21
+// left: 508
+// right: 582.53125
+// top: 149.375
+// width: 74.53125
+// x: 508
+// y: 149.375
+// __proto__: DOMRect
+
+// moveX(btn, 100, 1000, () => {                     //from here --
+//     moveX(btn, 100, 1000, () => {
+//         moveX(btn, 100, 1000, () => {
+//             moveX(btn, 100, 1000, () => {
+//                 moveX(btn, 1000, 1000)
+//             })
+//         })
+//     });
+// });                                              //to here is our callback, after callback, after callback
+
+
+// request()               //a request will accept two argumnets, a success call back and a failure callback
+                        //if the request works out here is the code that runs, if not here is also the code that will run 
+
+const moveX = (element, amount, delay, onSuccess, onFailure) => {
+    setTimeout(() => {
+    const bodyBoundary = document.body.clientWidth;
+    const elRight = element.getBoundingClientRect().right;
+    const currentLeft = element.getBoundingClientRect().left;
+    if (elRight + amount > bodyBoundary) {
+        onFailure();
+    } else {
+            element.style.transform = `translateX(${currentLeft + amount}px)`;
+            onSuccess();
+        };
+        
+    }, delay);
+};
+
+// moveX(btn, 100, 1000, () => {                     //from here --
+//     moveX(btn, 100, 1000, () => {
+//         moveX(btn, 100, 1000, () => {
+//             moveX(btn, 100, 1000, () => {
+//                 moveX(btn, 1000, 1000)
+//             })
+//         })
+//     });
+// });
+
+// so now we can invoke the function with a success callback and a failure callback, these will be two functions that are in order, 
+// first will be the success, and second will be the failure. 
+
+moveX(btn, 100, 1000, () => {
+    //success
+    moveX(btn, 400, 1000, () => {
+        //success
+        moveX(btn, 700, 1000, () => {
+            //success
+        }, () => {
+            //failure
+            alert('CANNOT MOVE!');
+        })
+    }, () => {
+        //failure
+        alert('CANNOT MOVE!');
+    })
+}, () => {
+    //callback failure
+    alert('CANNOT MOVE!');
+})
