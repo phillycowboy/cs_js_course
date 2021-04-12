d5fbbf9db9d3083f0c64fdabf71a5ff267faca07
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-04-12 17:09:42 -0400

    resolve and reject

--- async-code-callbacks-promises/resolving-rejecting-with-values-promises/app.js
+++ async-code-callbacks-promises/resolving-rejecting-with-values-promises/app.js
@@ -1,1 +1,31 @@
-alert('Does this work?');
+// alert('Does this work?');
+
+const fakeRequest = (url) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+                const pages = {
+                    '/users': [
+                        {id: 1, username: "Bilbo"},
+                        {id: 5, username: "Esmerelda"}
+                    ],
+                    '/about': 'This is the about page.'
+                };
+                const data = pages[url]
+                if(data){
+                    resolve({status: 200, data});
+                }else{
+                    reject({status: 404});
+                }
+        },1000);
+    });
+};
+
+
+fakeRequest("/users").then((res) => {
+    console.log('Status Code', res.status);
+    console.log('Data', res.data);
+    console.log("REQUEST WORKED!");
+}).catch((res) => {
+    console.log(res.status);
+    console.log("REQUEST FAILED!");
+})
