8db0dc14c7cc189dd4416299ea1d17b9d0b59fcd
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-12-08 16:59:29 -0500

    refactored axios request

--- axios-requests/app.js
+++ axios-requests/app.js
@@ -1,1 +1,41 @@
 console.log("%caxios-requests", "color: red")
+
+
+// url "https://swapi.dev/api/planets"
+
+const fetchNextPlanets = (url = 'https://swapi.dev/api/planets') => {
+    return axios.get(url)
+}
+const printPlanets = ({data}) => {
+    for(const planet of data.results){
+        console.log(planet.name)
+    }
+    return fetchNextPlanets(data.next)
+}
+
+fetchNextPlanets()
+.then(printPlanets)
+.then(fetchNextPlanets)
+.then(printPlanets)
+.catch((err) => {
+    console.log("ERROR!", err)
+})
+
+// axios.get('https://swapi.dev/api/planets')
+// .then(({data}) => {
+//     // console.log(data)
+//     for(const planet of data.results){
+//         console.log(planet.name)
+//     }
+    // return axios.get(data.next)
+    // return fetchNextPlanets(data.next)
+// })
+// .then(({data}) => {
+//     // console.log(data)
+//     for(const planet of data.results){
+//         console.log(planet.name)
+//     }
+// })
+// .catch((err) => {
+//     console.log("ERROR!", err)
+// })
