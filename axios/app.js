9afb05c6acf08a31f9dd8fbd652535c0e767f558
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-12-08 16:08:39 -0500

    finished axios basics

--- axios/app.js
+++ axios/app.js
@@ -2,3 +2,16 @@ console.log("welcome to axios");
 
 // external library for making http requests
 
+// unlike with fetch we do not have to do the json parsing on our own axios will parse it for us.
+
+// url "https://swapi.dev/api/planets"
+
+axios.get("https://swapi.dev/api/planets")
+.then((res) => {
+    // console.log(res);
+    // console.log(res.data);
+    console.log(res.data.results);
+})
+.catch((err) => {
+    console.log(err);
+})
