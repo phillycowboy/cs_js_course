fbed39d9bd3b119b261c5cfb047b5f8d7f9e7eaf
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-12-08 14:26:41 -0500

    refactored fetch request

--- xmlhttprequests-chaining-requests/app.js
+++ xmlhttprequests-chaining-requests/app.js
@@ -32,48 +32,97 @@
 
 // testing
 
+// const checkStatusAndResponse = (response) => {
+//     if(!response.ok)
+//     throw new Error(`Status Code Error: ${response.status}`)
+//     return response.json()
+// }
+
+// const printPlanets = (data) => {
+//     console.log("Loaded 10 more planets.")
+//     for(let planet of data.results){
+//         console.log(planet.name)
+//     }
+//     // const p = new Promise((resolve, reject) => {
+//     //     resolve(data)
+//     // })
+//     // return p
+//     return Promise.resolve(data.next);
+// }
+
+// const get10MorePlanets = (url= "https://swapi.dev/api/planets") => {
+//     return fetch(url);
+// }
+
+// fetch("https://swapi.dev/api/planets")
+// .then(checkStatusAndResponse)
+// .then(printPlanets)
+// .then(get10MorePlanets)
+// .then(checkStatusAndResponse)
+// .then(printPlanets)
+// .catch((err) => {
+//     console.log("SOMETHING WENT WRONG WITH FETCH")
+//     console.log(err)
+// })
+    // console.log("Feteched first 10 planets")
+    // for(let planet of data.results){
+    //     console.log(planet.name)
+    // }
+    // const nextUrl = data.next;
+    // return fetch(nextUrl);
+    // const filmURL = data.results[0].films[0];
+    // for(let planet of data.results){
+    //     console.log(planet)
+    //     console.log(planet.name)
+    // }
+// })
+// .then(checkStatusAndResponse)
+// .then(printPlanets)
+// .catch((err) => {
+//     console.log("SOMETHING WENT WRONG WITH FETCH")
+//     console.log(err)
+// })
+
+// refactored fetch with built in callback functions
+
+// fetch("https://swapi.dev/api/planets")
+// .then(checkStatusAndResponse)
+// .then(printPlanets)
+// .then(get10MorePlanets)
+// .then(checkStatusAndResponse)
+// .then(printPlanets)
+// .then(get10MorePlanets)
+const url = "https://swapi.dev/api/planets"
+
 const checkStatusAndResponse = (response) => {
     if(!response.ok)
     throw new Error(`Status Code Error: ${response.status}`)
     return response.json()
 }
 
-fetch("https://swapi.dev/api/planets")
-.then(checkStatusAndResponse)
-.then((data) => {
-    console.log("Feteched first 10 planets")
+const printPlanets = (data) => {
+    console.log("Loaded 10 more planets.")
     for(let planet of data.results){
         console.log(planet.name)
     }
-    const nextUrl = data.next;
-    return fetch(nextUrl);
-    // const filmURL = data.results[0].films[0];
-    // for(let planet of data.results){
-    //     console.log(planet)
-    //     console.log(planet.name)
-    // }
-})
+    // const p = new Promise((resolve, reject) => {
+    //     resolve(data)
+    // })
+    // return p
+    return Promise.resolve(data.next);
+}
+
+const get10MorePlanets = (url) => {
+    return fetch(url);
+}
+
+fetch(url)
 .then(checkStatusAndResponse)
-.then((data) => {
-    console.log("Fetched next ten planets")
-    for(let planet of data.results){
-        console.log(planet.name)
-    }
-    // const nextUrl = data.next;
-    // return fetch(nextUrl);
-    // console.log(data.title);
-})
+.then(printPlanets)
+.then(get10MorePlanets)
+.then(checkStatusAndResponse)
+.then(printPlanets)
 .catch((err) => {
     console.log("SOMETHING WENT WRONG WITH FETCH")
     console.log(err)
 })
-
-// refactored fetch with built in callback functions
-
-fetch("https://swapi.dev/api/planets")
-.then(checkStatusAndResponse)
-.then(printPlanets)
-.then(get10MorePlanets)
-.then(checkStatusAndResponse)
-.then(printPlanets)
-.then(get10MorePlanets)
