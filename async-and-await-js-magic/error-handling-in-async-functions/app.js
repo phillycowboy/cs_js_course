2a41776ed3eb23fa2c461e1401fb036aacb80255
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-12-09 14:47:34 -0500

    finished error handling

--- async-and-await-js-magic/error-handling-in-async-functions/app.js
+++ async-and-await-js-magic/error-handling-in-async-functions/app.js
@@ -1,4 +1,28 @@
 console.log("error handling in async functions");
 
 // error handling in async functions 
+// how do we handle if a promise is rejected?
 
+
+// async function getPlanets(){
+//     const res = await axios.get("https://swapi.dev/api/planetsssssssss")
+//     console.log(res.data)
+// }
+
+// getPlanets().catch((err) => {
+//     console.log("In CATCH!")
+//     console.log(err)
+// })
+
+// try and catch 
+
+async function getPlanets(){
+    try{
+        const res = await axios.get("https://swapi.dev/api/planets")
+        console.log(res.data)
+    }catch (err){
+        console.log('IN CATCH!!!', err)
+    }
+
+}
+getPlanets();
