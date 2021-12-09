45a73310a9f6a13f0507eed4fdb86a8875839c6d
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-12-09 14:20:16 -0500

    finished with await

--- async-and-await-js-magic/await-keyword/app.js
+++ async-and-await-js-magic/await-keyword/app.js
@@ -1,3 +1,18 @@
 console.log("%cawait keyword", "color: orange");
 
-// start here either tomorrow or on monday or when you have free time! 
+// await will pause the execution of a function waiting for a promise to be resolved.
+
+// function getPlanets(){
+//     return axios.get("https://swapi.dev/api/planets");
+// }
+
+// getPlanets().then((res) => {
+//     console.log(res.data)
+// })
+
+async function getPlanets(){
+    const res = await axios.get("https://swapi.dev/api/planets")
+    console.log(res.data)
+}
+
+getPlanets();
