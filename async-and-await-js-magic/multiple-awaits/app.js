fd08bf350744032d2943a7bd9f75e48e254ed374
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-12-15 15:06:03 -0500

    added function to move button certain number of times

--- async-and-await-js-magic/multiple-awaits/app.js
+++ async-and-await-js-magic/multiple-awaits/app.js
@@ -1,2 +1,43 @@
 console.log("%cmultiple awaits!", "color: pink");
 
+// moveX returns a promise, and in this promise it will move an element, a certain number of pixels(ammount), a certain number of seconds(delay) depending on whether the 
+// number is positive or negative
+
+const moveX = (element, amount, delay) => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const bodyBoundary = document.body.clientWidth;
+			const elRight = element.getBoundingClientRect().right;
+			const currLeft = element.getBoundingClientRect().left;
+			if (elRight + amount > bodyBoundary) {
+				reject({ bodyBoundary, elRight, amount });
+			}
+			else {
+				element.style.transform = `translateX(${currLeft + amount}px)`;
+				resolve();
+			}
+		}, delay);
+	});
+};
+
+const btn = document.querySelector('button');
+moveX(btn, 100, 1000)
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.then(() => moveX(btn, 100, 1000))
+	.catch(({ bodyBoundary, amount, elRight }) => {
+		console.log(`Cannot Move! Body is ${bodyBoundary}px wide`);
+		console.log(`Element is at ${elRight}px, ${amount}px is too large!`);
+	});
