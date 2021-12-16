bdfbe20f9f43a3fa622739f7b0985c0f889d0390
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-12-15 22:23:03 -0500

    added notes

--- async-and-await-js-magic/paralell-vs-sequential-requests/app.js
+++ async-and-await-js-magic/paralell-vs-sequential-requests/app.js
@@ -1,1 +1,19 @@
 console.log("paralell vs. sequential requests");
+
+// api https://pokeapi.co/api/v2/pokemon
+
+async function get3Pokemon(){
+    // this line will run and fetch the data
+    const poke1 = await axios.get("https://pokeapi.co/api/v2/pokemon/1");
+    // then once this returns this line will run 
+    const poke2 = await axios.get("https://pokeapi.co/api/v2/pokemon/2");
+    // then once this returns this line will run 
+    const poke3 = await axios.get("https://pokeapi.co/api/v2/pokemon/3");
+    // they are not running in paralell they are running in sequence.
+    // sequential requests.
+    console.log(poke1.data);
+    console.log(poke2.data);
+    console.log(poke3.data);
+}
+
+get3Pokemon();
