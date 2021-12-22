db1ffdba36707901bd1bebdd5640fe9d7226c814
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-12-22 16:02:19 -0500

    finished factory functions

--- prototypes-classes-and-the-new-operator/intro-to-oop/app.js
+++ prototypes-classes-and-the-new-operator/intro-to-oop/app.js
@@ -17,9 +17,18 @@ function makeColor(r,g,b){
     color.r = r;
     color.g = g;
     color.b = b;
+    color.rgb = function(){
+        const {r,g,b} = this;
+        return `rgb(${r}, ${g}, ${b})`;
+    };
+    color.hex = function(){
+        const {r,g,b} = this;
+        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    };
     return color;
 };
 
+const firstColor = makeColor(35,255,150);
 // will print out a hexadecimal color for us when the aruments are passed in 
 
 // console.log(hex(255, 100, 25));
