adb950f93d594383c14e61f76290c7da17f04923
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-12-22 14:56:29 -0500

    making color object

--- prototypes-classes-and-the-new-operator/intro-to-oop/app.js
+++ prototypes-classes-and-the-new-operator/intro-to-oop/app.js
@@ -4,5 +4,24 @@ console.log("INTRO TO OOP!");
 // FACTORY FUNCTIONS
 
 function hex(r, g, b){
-    return '#' + 
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 }
+
+
+function rgb(r,g,b){
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+function makeColor(r,g,b){
+    const color = {};
+    color.r = r;
+    color.g = g;
+    color.b = b;
+    return color;
+};
+
+// will print out a hexadecimal color for us when the aruments are passed in 
+
+// console.log(hex(255, 100, 25));
+// "#ff6419"
+// "rgb(255, 100, 25)"
