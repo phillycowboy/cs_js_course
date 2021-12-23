3f137dcc9c31f8e24f4eef4b5520936f4c705b84
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-12-23 14:47:55 -0500

    finished with intro to OOP

--- prototypes-classes-and-the-new-operator/intro-to-oop/cats.js
+++ prototypes-classes-and-the-new-operator/intro-to-oop/cats.js
@@ -1,6 +1,7 @@
 // Cats.JS
 class Pet {
     constructor(name, age){
+        console.log('In PET CONSTRUCTOR');
         this.name = name;
         this.age = age;
     }
@@ -12,17 +13,28 @@ class Pet {
 
 
 class Cat extends Pet{
+    constructor(name, age, livesleft=9){
+        // super references the class that we are extending from no need to duplicate the constructor when you already have 
+        // that functionality built in! 
+        console.log('In CAT CONSTRUCTOR');
+        super(name, age)
+        this.livesleft = livesleft;
+    }
     meow(){
         return 'MEOWWWW!'
     }
 }
 
-const monty = new Cat('monty', 9);
+// const monty = new Cat('monty', 9);
 
 class Dog extends Pet{
     woof(){
         return 'WOOFFFFF!'
     }
+    // has both an extended eat, and its own, it will run this function if you call it on the class of a Dog;
+    eat(){
+        return `${this.name} scrafs his food!`;
+    }
 }
 
-const wyatt = new Dog('wyatt', 13);
+// const wyatt = new Dog('wyatt', 13);
