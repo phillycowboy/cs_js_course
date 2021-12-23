e6942e24a0f2f509bcdfe1c0d0723208c45a6a81
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-12-22 21:50:26 -0500

    refactored to prototype functions instead of creating function for the instance

--- prototypes-classes-and-the-new-operator/intro-to-oop/app.js
+++ prototypes-classes-and-the-new-operator/intro-to-oop/app.js
@@ -37,14 +37,69 @@ console.log("INTRO TO OOP!");
 
 // CONSTRUCTOR FUNCTIONS 
 
+// function Color(r,g,b){
+//     this.r = r;
+//     this.g = g;
+//     this.b = b;
+//     this.rgb = function(){
+//         const {r,g,b} = this;
+//         return `rgb(${r}, ${g}, ${b})`;
+//     };
+//     console.log(this);
+// }
+// without the new operator this is referring to the window
+
+// new Color(255,40,0);
+// with the new operator it is referring to the Color object and implictly makes it own object 
+
+// should look like this in console after we instatiate a new object: 
+// Color {r: 255, g: 10, b: 40, rgb: ƒ}
+// b: 40
+// g: 10
+// r: 255
+// rgb: ƒ ()
+// arguments: null
+// caller: null
+// length: 0
+// name: ""
+// prototype: {constructor: ƒ}
+// [[FunctionLocation]]: app.js:44
+// [[Prototype]]: ƒ ()
+// [[Scopes]]: Scopes[1]
+// [[Prototype]]: Object
+
+// still an indiviual function so we need to define it on the prototype itself: 
+
 function Color(r,g,b){
     this.r = r;
     this.g = g;
     this.b = b;
+    // this.rgb = function(){
+    //     const {r,g,b} = this;
+    //     return `rgb(${r}, ${g}, ${b})`;
+    // };
     console.log(this);
 }
-// without the new operator this is referring to the window
 
-new Color(255,40,0);
-// with the new operator it is referring to the Color object and implictly makes it own object 
+Color.prototype.rgb = function(){
+    const {r,g,b} = this;
+    return `rgb(${r}, ${g}, ${b})`;
+};
 
+const color1 = new Color(255,40,0);
+
+// now we have rgb not defined on the object itself but in the prototype of the Color class and it should look like this as opposed to above: 
+
+// Color {r: 255, g: 40, b: 0}
+// b: 0
+// g: 40
+// r: 255
+// [[Prototype]]: Object
+// rgb: ƒ ()
+// constructor: ƒ Color(r,g,b)
+// [[Prototype]]: Object
+
+// we can still call color1.rgb(): 
+
+// color1.rgb()
+// 'rgb(255, 40, 0)'
