81a99f5595f6c33ab86e9d1ed0e8a658053a8544
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-12-22 22:51:21 -0500

    added changes and new instances, and notes

--- prototypes-classes-and-the-new-operator/intro-to-oop/app.js
+++ prototypes-classes-and-the-new-operator/intro-to-oop/app.js
@@ -1,8 +1,8 @@
 // INTRO TO OOP! 
 console.log("INTRO TO OOP!");
-
+// *****************
 // FACTORY FUNCTIONS
-
+// *****************
 // function hex(r, g, b){
 //     return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 // }
@@ -35,8 +35,10 @@ console.log("INTRO TO OOP!");
 // "#ff6419"
 // "rgb(255, 100, 25)"
 
-// CONSTRUCTOR FUNCTIONS 
 
+// *******************************************
+// CONSTRUCTOR FUNCTIONS  and the new Operator
+// *******************************************
 // function Color(r,g,b){
 //     this.r = r;
 //     this.g = g;
@@ -86,7 +88,28 @@ Color.prototype.rgb = function(){
     return `rgb(${r}, ${g}, ${b})`;
 };
 
-const color1 = new Color(255,40,0);
+Color.prototype.hex = function(){
+    const {r,g,b} = this;
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+Color.prototype.rgba = function(a=1.0){
+    const {r,g,b} = this;
+    return `rgba(${r}, ${g}, ${b}, ${a})`
+
+}
+
+const color1 = new Color(60,255,60);
+// function calls and their results 
+// color1.rgb = 'rgb(60, 255, 60)'
+// color1.hex = '#3cff3c'
+// color1.rgba = 'rgba(60, 255, 60, 1)'
+
+// and we can change the back ground color of the body by calling one of these functions: 
+// document.body.style.backgroundColor = color1.rgb()
+// 'rgb(60, 255, 60)'
+// and it should change! 
+const color2 = new Color(0,0,0);
 
 // now we have rgb not defined on the object itself but in the prototype of the Color class and it should look like this as opposed to above: 
 
@@ -103,3 +126,18 @@ const color1 = new Color(255,40,0);
 
 // color1.rgb()
 // 'rgb(255, 40, 0)'
+
+// when we call color1.hex() or color2.hex() we get two different values bc the value of THIS changes and should get this. not only that they should be one function and 
+// function only on the prototype and not an individual function for the instance. hence they should be true when equated and not false so you should see this: 
+
+// color1.hex()
+// '#ff2800'
+// color2.hex()
+// '#000000'
+
+// color1.hex === color2.hex
+// true
+
+
+// you should see both function defind on the prototype and not the object itself. 
+
