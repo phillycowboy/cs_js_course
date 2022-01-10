8075ae957efb7dce9c6417fdac01e14c5fc2f6d8
Ben Harter-Murphy <benhartermurphy@gmail.com>
2022-01-10 11:27:43 -0500

    added start function and queryselectors for DOM elements

--- drawing-animations/timer/index.js
+++ drawing-animations/timer/index.js
@@ -1,8 +1,22 @@
 // console.log("Hi There!");
 class Timer {
-    constructor(message){
-        console.log(message);
+    constructor(durationInput, startButton, pauseButton){
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener('click', this.start)
+    }
+
+    start(){
+        console.log('Time to start the timer');
     }
 }
 // when we call a new instance the constructor is auotmatically run for us.
-new Timer('hi there!');
+// new Timer('hi there!');
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
