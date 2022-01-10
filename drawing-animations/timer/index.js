1a8dd3b8471f22dd5726257f5342a99efe007298
Ben Harter-Murphy <benhartermurphy@gmail.com>
2022-01-10 10:57:57 -0500

    started new class

--- drawing-animations/timer/index.js
+++ drawing-animations/timer/index.js
@@ -1,1 +1,8 @@
-console.log("Hi There!");
+// console.log("Hi There!");
+class Timer {
+    constructor(message){
+        console.log(message);
+    }
+}
+// when we call a new instance the constructor is auotmatically run for us.
+new Timer('hi there!');
